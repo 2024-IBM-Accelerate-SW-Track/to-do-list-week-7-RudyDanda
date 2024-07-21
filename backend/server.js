b19c9029f1180dc5e2294e0043e77461261a4641
RudyDanda <anirudh.danda@gmail.com>
2024-07-21 15:44:52 -0400

    Added basic auth and frontend login

--- backend/server.js
+++ backend/server.js
@@ -8,30 +8,6 @@ const fsPromises = require("fs").promises;
 const todoDBName = "tododb";
 const useCloudant = true;
 
-
-
-//Init code for Cloudant
-const {CloudantV1} = require('@ibm-cloud/cloudant');
-if (useCloudant)
-{
-    initDB();
-}
-
-
-app.use(cors());
-app.use(bodyParser.json({ extended: true }));
-
-app.listen(port, () => console.log("Backend server live on " + port));
-
-
-
-app.get("/", (request, response) => {
-    response.send({ message: "Connected to Backend server!" });
-});
-
-//add new item to json file
-app.post("/add/item", addItem)
-
 async function addItem (request, response) {
     try {
         // Converting Javascript object (Task Item) to a JSON string
@@ -162,3 +138,63 @@ async function initDB ()
 
   }
 };
+
+const basicAuth = require("express-basic-auth");
+var { authenticator, upsertUser, cookieAuth } = require("./authentication");
+const auth = basicAuth({
+    authorizer: authenticator
+});
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
+
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+
+app.get("/authenticate", auth, (req, res) => {
+  console.log(`user logging in: ${req.auth.user}`);
+  res.cookie('user', req.auth.user, { signed: true });
+  res.sendStatus(200);
+});
+
+app.post("/users", (req, res) => {
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+  const upsertSucceeded = upsertUser(username, password)
+  res.sendStatus(upsertSucceeded ? 200 : 401);
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie('user');
+  res.end();
+});
+
+app.post("/items", cookieAuth, addItem);
+
+app.get("/items", cookieAuth, getItems);
+
+app.get("/items/search", cookieAuth, searchItems);
+
+//Init code for Cloudant
+const {CloudantV1} = require('@ibm-cloud/cloudant');
+if (useCloudant)
+{
+    initDB();
+}
+
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+
+app.listen(port, () => console.log("Backend server live on " + port));
+
+
+
+app.get("/", (request, response) => {
+    response.send({ message: "Connected to Backend server!" });
+});
+
+//add new item to json file
+app.post("/add/item", addItem)
